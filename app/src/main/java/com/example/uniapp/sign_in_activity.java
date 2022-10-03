738c18338ebbcbdce8b3c21d7ee6fc73cbb3e9ed
Bhupender <bhupender.contact@gmail.com>
2022-10-03 23:41:05 +0530

    Login & Signup Screen Desgin

--- app/src/main/java/com/example/uniapp/sign_in_activity.java
+++ app/src/main/java/com/example/uniapp/sign_in_activity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class sign_in_activity extends AppCompatActivity {
     private TextView signUp_page;
+    private EditText email;
+    private EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_activity);
-        signUp_page = findViewById(R.id.signUp_page);
 
+        // ********* To Swap Between Sign Up & Sign In Screen *********
+        signUp_page = findViewById(R.id.signUp_page);
         signUp_page.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +27,6 @@ public class sign_in_activity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        // ********* End *********
     }
 }
--- app/src/main/java/com/example/uniapp/sign_up_activity.java
+++ app/src/main/java/com/example/uniapp/sign_up_activity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class sign_up_activity extends AppCompatActivity {
     private TextView signIn_page;
+    private EditText email;
+    private EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_activity);
-        signIn_page = findViewById(R.id.signIn_page);
 
+        // ********* To Swap Between Sign Up & Sign In Screen *********
+        signIn_page = findViewById(R.id.signIn_page);
         signIn_page.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +27,6 @@ public class sign_up_activity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        // ********* End *********
     }
 }
