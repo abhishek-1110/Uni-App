39558dd33b063882a1cc51487b72d30b2dba0c5e
Bhupender <bhupender.contact@gmail.com>
2022-10-03 23:24:25 +0530

    Login & Signup Screen Desgin

--- app/src/main/java/com/example/uniapp/sign_up_activity.java
+++ app/src/main/java/com/example/uniapp/sign_up_activity.java
@@ -2,13 +2,25 @@ package com.example.uniapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class sign_up_activity extends AppCompatActivity {
-
+    private TextView signIn_page;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_activity);
+        signIn_page = findViewById(R.id.signIn_page);
+
+        signIn_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(sign_up_activity.this, sign_in_activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
